4e786f8306020046a0895840ae94bc728f26c2ff
erikbenton <erik.benton@hotmail.com>
2017-11-29 19:59:30 -0500

    changing Player/Room classes to structures in Dungeon class since they only hold info at the moment

--- Dungeon.rb
+++ Dungeon.rb
@@ -1,8 +1,17 @@
+
+#Now using structures instead of classes
+#require_relative "Player.rb"
+#require_relative "Room.rb"
+
 class Dungeon
 	attr_accessor :player
 
 	def initialize(player_name)
-		@player = Playernew(player_name)
+		@player = Player.new(player_name)
 		@rooms = []
 	end
+
+	Player = Struct.new(:name, :location)
+	Room = Struct.new(:reference, :name, :description, :connections)
+
 end
---
+++ Game.rb
@@ -0,0 +1,5 @@
+require_relative "Dungeon.rb"
+
+my_dungeon = Dungeon.new("Erik")
+
+puts my_dungeon.player.name
